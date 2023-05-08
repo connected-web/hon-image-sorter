293015f92f68f97d6b0f29c8752ba74db2b060b9
John Beech <johnbeech.github@mkv25.net>
2023-05-08 21:14:05 +0100

    Initial exploratory proof of concept

---
+++ components/hon-browser/src/clients/honClient.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export default class HonClient {
+    constructor(serverUrl) {
+        this.client = axios.create({
+            baseURL: serverUrl
+        })
+    }
+
+    async serverStatus() {
+        const response = await this.client.get('/')
+        return response.data
+    }
+
+    async listFiles() {
+        const response = await this.client.get('/server/list')
+        return response.data
+    }
+}
---
+++ components/hon-server/start.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import cors from 'cors'
+
+import { find, position } from 'promise-path'
+
+const app = express()
+app.use(cors())
+
+const sourcePath = process.env.HON_IMAGE_PATH ?? process.cwd()
+const serverPort = 8901
+
+const sourcePos = position(sourcePath)
+const basePath = sourcePos('./')
+
+app.get('/', (req, res) => {
+  console.log('GET /')
+  res.json({
+    message: 'Hello',
+    sourcePath
+  })
+})
+app.get('/server/list', async (req, res) => {
+  console.log('GET /server/list')
+  const folderSearch = sourcePos('**\/')
+  const imageSearch = sourcePos('**\/*.png')
+  console.log('List files on server', { imageSearch })
+  const files = await find(imageSearch)
+  const folders = await find(folderSearch)
+  const images = files.map(filepath => filepath.replace(basePath, '/images/'))
+  res.json({
+    sourcePath,
+    folders: folders.map(folderpath => folderpath.replace(basePath, '/images/')),
+    images
+  })
+})
+console.log('Source path for /images:', { sourcePath })
+app.use('/images', express.static(sourcePath))
+
+const uiServer = 'http://localhost:5173'
+console.log(`UI Server running on ${uiServer}`)
+
+app.listen(serverPort, () => {
+    console.log(`HON Server running on http://localhost:${serverPort}`)
+})
